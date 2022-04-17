89df125cc2a0385ff8f92aa44b68b78eb60c6f89
icakhi <hanagotei.icakhi@gmail.com>
2022-04-18 01:21:40 +0900

    [Maya.ScriptRunner] スクリプトの説明を追加

--- Maya/scripts/SeedlingTools/Scripting/ScriptRunner/core.py
+++ Maya/scripts/SeedlingTools/Scripting/ScriptRunner/core.py
@@ -1,5 +1,24 @@
 # -*- coding: utf_8 -*-
 
+# ****************************************************************
+# SeedlingTools - ScriptRunner
+# version: 0.2.0
+# ****************************************************************
+# This tool is license-free and may be used commercially and modified for personal use only.
+# However, secondary distribution, sale, or transfer of modified data is prohibited. Please do not do so.
+# Licenses are subject to change in the future. We will announce any changes on Twitter.
+
+# Please read the initial release document for more information:
+# https://github.com/Icakhi/SeedlingTools-release/releases/tag/v0.1.0
+
+# This is a pre-release and may have some issues.
+# If you encounter any errors, please contact me.
+# ****************************************************************
+# Created by: icakhi
+# Twitter: @icakhi
+# Github: https://github.com/Icakhi/SeedlingTools-release
+# ****************************************************************
+
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import division
